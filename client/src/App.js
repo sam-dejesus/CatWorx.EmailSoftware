baebf41eac9d7a89d62cf8e11acee5a773e11a86
samuel de jesus <samuel1de2jesus3@gmail.com>
2023-10-03 13:58:16 -0400

    created the structure for the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Login from './pages/login/Login'
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+
+      <Router>
+
+          
+          <div className="container master">
+            <Routes>
+              {/* <Route path="request" element={<Request />} /> */}
+              <Route path="/" element={<Login />} />
+              {/* <Route path="/search" element={<SearchBar />} />
+              <Route path="/home" element={<Home />} /> */}
+
+              {/* <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/profiles/:username" element={<Profile />} /> */}
+            </Routes>
+          </div>
+     
+
+      </Router>
+      
+    </ApolloProvider>
   );
+
 }
 
 export default App;
---
+++ client/src/pages/login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+
+export default function Login() {
+ return (
+    <main className="">
+   
+    </main>
+  );
+
+
+            }
